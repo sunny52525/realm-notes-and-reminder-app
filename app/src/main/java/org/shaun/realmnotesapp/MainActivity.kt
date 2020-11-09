77fccd958dacc71eafbb86d9f90d36aee17ab912
Sunny <30768018+sunny52525@users.noreply.github.com>
2020-11-09 19:46:04 +0530

    added night mode switch

--- app/src/main/java/org/shaun/realmnotesapp/MainActivity.kt
+++ app/src/main/java/org/shaun/realmnotesapp/MainActivity.kt
@@ -2,11 +2,15 @@ package org.shaun.realmnotesapp
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import android.content.res.Configuration
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.Observer
@@ -17,6 +21,7 @@ import com.google.android.material.floatingactionbutton.ExtendedFloatingActionBu
 import io.realm.Realm
 import io.realm.RealmChangeListener
 import io.realm.RealmResults
+import kotlinx.android.synthetic.main.activity_main.*
 import org.shaun.realmnotesapp.Dao.NotesDao
 import org.shaun.realmnotesapp.adapters.NotesAdapter
 import org.shaun.realmnotesapp.fragment.FullNotesFragment
@@ -26,6 +31,8 @@ import org.shaun.realmnotesapp.viewModel.NotesViewModel
 
 private const val TAG = "Main Activity"
 
+private var darkMode = true
+
 class MainActivity : AppCompatActivity(), NotesAdapter.OnHolderClick {
     private lateinit var notesViewModel: NotesViewModel
     private var db: RealmResults<NotesObject>? = null
@@ -34,6 +41,7 @@ class MainActivity : AppCompatActivity(), NotesAdapter.OnHolderClick {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         createChannel("reminder", "reminder")
+        initTheme()
         val recyclerView = findViewById<RecyclerView>(R.id.notes_recycler_view)
 
         val adapter = NotesAdapter(this, this)
@@ -52,6 +60,26 @@ class MainActivity : AppCompatActivity(), NotesAdapter.OnHolderClick {
         new_note.setOnClickListener {
             startActivity(Intent(this, NewNoteActivity::class.java))
         }
+
+        nightMode.setOnCheckedChangeListener { buttonView, isChecked ->
+            val isNightTheme =
+                resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+            when (isNightTheme) {
+                Configuration.UI_MODE_NIGHT_YES -> {
+                    darkMode = false
+                }
+                Configuration.UI_MODE_NIGHT_NO -> {
+                    darkMode = true
+                }
+            }
+
+            nightMode(darkMode)
+            val themePreference:SharedPreferences=getSharedPreferences("userTheme", Context.MODE_PRIVATE)
+            val editor=themePreference.edit()
+            editor.putBoolean("userTheme", darkMode)
+            editor.apply()
+        }
+
 
         db = NotesDao(Realm.getDefaultInstance()).getAllNotesAll()
         db?.addChangeListener(RealmChangeListener {
@@ -97,6 +125,32 @@ class MainActivity : AppCompatActivity(), NotesAdapter.OnHolderClick {
         transaction.replace(R.id.main_activity, newCustomFragment)
         transaction.addToBackStack(null)
         transaction.commit()
+    }
+
+    private fun initTheme(){
+        val themepref: SharedPreferences =getSharedPreferences("userTheme", Context.MODE_PRIVATE)
+        val editor=themepref.edit()
+
+        if(themepref.contains("userTheme")){
+            darkMode = themepref.getBoolean("userTheme",false)
+        }else{
+            val isNightTheme =
+                resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+            darkMode = isNightTheme== Configuration.UI_MODE_NIGHT_YES
+            editor.putBoolean("userTheme",false)
+            editor.apply()
+        }
+        nightMode(darkMode)
+
+    }
+
+    private fun nightMode(on:Boolean){
+        nightMode.isChecked= on
+        if(on)
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        else
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
     }
 
 }
